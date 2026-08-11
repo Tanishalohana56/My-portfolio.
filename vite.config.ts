c092a94abba840ded729fab2c53cf84983314ccc
Tanishalohana56 <tanulohana51@gmail.com>
2026-08-11 18:16:07 +0500

    Fix Vercel and GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/My-portfolio./',
+  base: process.env.GITHUB_ACTIONS ? '/My-portfolio./' : '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
